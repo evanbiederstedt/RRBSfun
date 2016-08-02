a524cb81bab02040ac7e3d55c0bc31e288054e60
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-08-02 09:56:58 -0400

    updated

--- scripts/eiphen.py
+++ scripts/eiphen.py
@@ -15,13 +15,13 @@ df = pd.read_table("RRBS_NormalBCD19pCD27mcell1_22_CGAGGCTG.GTTGAG.bed.anno")
 
 df = df.drop(df.ix[:,'bedCol.2': 'hypoInHues64Distance'], axis=1)
 df = df.drop("bedCol", 1)
-df = df.rename(columns = {'bedCol.1':'Methyl'})
+df = df.rename(columns = {'bedCol.1':"Methyl"})
 
 df["Methyl"] = df["Methyl"].str.replace(r'[\'\"]', '')
-df["Meth"] = df.Meth.apply(eval)
-df.loc[df['Meth'] >= 0.75, 'Meth'] = 1
-df.loc[df['Meth'] <= 0.25, 'Meth'] = 0
-df.loc[(df["Meth"] < 0.75) & (df["Meth"] > 0.25), 'Meth'] = str('NaN')
+df["Methyl"] = df.Meth.apply(eval)
+df.loc[df["Methyl"] >= 0.75, "Methyl"] = 1
+df.loc[df["Methyl"] <= 0.25, "Methyl"] = 0
+df.loc[(df["Methyl"] < 0.75) & (df["Methyl"] > 0.25), "Methyl"] = str('NaN')
 
 df.shape
 
--- scripts/epiphen_all.py
+++ scripts/epiphen_all.py
@@ -10,17 +10,17 @@ pd.set_option('display.max_columns', 50) # print all rows
 import os
 os.chdir("/Users/evanbiederstedt/Downloads/phylo_files")
 
-cw154 = glob.glob("RRBS_cw154*")
+all_files = glob.glob("RRBS*")
 
-for filename in cw154:
+for filename in all_files:
     df= pd.read_table(filename)
     df['filename'] = str('filename')
     df = df.drop(df.ix[:,'bedCol.2': 'hypoInHues64Distance'], axis=1)
     df = df.drop("bedCol", 1)
-    df = df.rename(columns = {'bedCol.1':'Methyl'})
+    df = df.rename(columns = {'bedCol.1':"Methyl"})
     df["Methyl"] = df["Methyl"].str.replace(r'[\'\"]', '')
-    df["Meth"] = df.Meth.apply(eval)
-    df.loc[df['Meth'] >= 0.75, 'Meth'] = 1
-    df.loc[df['Meth'] <= 0.25, 'Meth'] = 0
-    df.loc[(df["Meth"] < 0.75) & (df["Meth"] > 0.25), 'Meth'] = str('NaN')
+    df["Methyl"] = df.Methyl.apply(eval)
+    df.loc[df["Methyl"] >= 0.75, "Methyl"] = 1
+    df.loc[df["Methyl"] <= 0.25, "Methyl"] = 0
+    df.loc[(df["Methyl"] < 0.75) & (df["Methyl"] > 0.25), "Methyl"] = str("NaN")
     df.to_csv(str("phyl_") + str(filename) +str(".csv"))
