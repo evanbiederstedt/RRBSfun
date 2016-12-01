8bf74f04672b10444392e57f46c626a2effd12fc
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-11-30 23:52:55 -0500

    updated script

--- scripts/repeat_finder.py
+++ scripts/repeat_finder.py
@@ -4,12 +4,12 @@ import pandas as pd
 import os
 import dask.dataframe as dd
 
-repeats = dd.read_csv("RRBS_repeats.csv")
+repeats = dd.read_csv("repeats_hg19.csv")
 
 anno = dd.read_table("RRBS_NormalBCD19pCD27pcell1_22_TAGGCATG.CATGAC.dan.anno")
 
 df1 = dd.merge(anno, repeats, on="chr", how="outer", suffixes=("","_repeat"))
-df1.to_csv("find_repeatsTEST1014.csv", index=False)
+df1.to_csv("find_repeatsTESToutput.csv", index=False)
 df1 = df1[(repeats.chr == row.chr) & (anno.start >= repeats.begin) & (anno.start <= repeats.end)]
 df1 = dd.merge(anno, df1, on = ["chr"])
-df1.to_csv("find_repeats1014.csv", index=False).compute(num_workers=20)
+df1.to_csv("find_repeatsTEST2.csv", index=False).compute(num_workers=20)
