9e818286790c8d1c753ed683a57cb80042aa02ff
Evan Biederstedt <evan.biederstedt@gmail.com>
2016-12-02 18:49:48 -0500

    update

---
+++ scripts/second_repeat_finder.py
@@ -0,0 +1,21 @@
+import glob
+import numpy as np
+import pandas as pd
+import os
+
+df1 = pd.read_csv("repeats_hg19.csv")
+
+anno = pd.read_table("RRBS_NormalBCD19pCD27pcell1_22_TAGGCATG.CATGAC.dan.anno")
+
+
+def match_labels(row):
+    curr_df = df1[ (df1["chr"]==row["chr"]) & (df1["start"]<=row["start"]) & (df1["end"]>=row["start"]) ]
+    try:
+        row["repeat_class"] = curr_df["repeat_class"].iloc[0]
+    except:
+        row["repeat_class"] = np.NaN
+    return row
+
+result = anno.apply(lambda x:match_labels(x), axis=1)
+
+result.to_csv("test101.csv", index=False)
